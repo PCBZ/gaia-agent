cd88a53ed5c18feea30ca06038e522b189ba6978
PCBZ <devil02047@outlook.com>
2026-08-25 21:13:00 -0700

    add question5

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 
 from q1 import Q1
 from q3 import Q3
+from q5 import Q5
 from q6 import Q6
 from q9 import Q9
 from q12 import Q12
@@ -13,7 +14,7 @@ from q17 import Q17
 from q19 import Q19
 
 # Instantiate each solver; key the registry by its GAIA question number.
-SOLVERS = {s.number: s for s in [Q1(), Q3(), Q6(), Q9(), Q12(), Q13(), Q17(), Q19()]}
+SOLVERS = {s.number: s for s in [Q1(), Q3(), Q5(), Q6(), Q9(), Q12(), Q13(), Q17(), Q19()]}
 
 
 def main() -> None:
